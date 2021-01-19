6effb254a073970e4e892e75bce2c120132b8b45
Ralph Mallett <rmallett12@gmail.com>
2021-01-19 14:20:41 +0000

    First commit: setup project

---
+++ src/test/java/AirportTest.java
@@ -0,0 +1,13 @@
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AirportTest {
+
+    private Airport underTest;
+
+    @BeforeEach
+    void setUp() {
+        underTest = new Airport();
+    }
+}
