9f01e2fc731420f4997e39477d132e0346d7c302
Ralph Mallett <rmallett12@gmail.com>
2021-01-21 10:40:44 +0000

    Refactor: create landed planes variable

--- src/test/java/AirportTest.java
+++ src/test/java/AirportTest.java
@@ -1,23 +1,27 @@
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.ArrayList;
+
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 public class AirportTest {
 
     private Airport airportUnderTest;
     private Plane planeUnderTest;
+    private ArrayList<Plane> landedPlanes;
 
     @Before
     public void setUp() {
         airportUnderTest = new Airport();
         planeUnderTest = new Plane();
+        landedPlanes = airportUnderTest.getPlanes();
     }
 
     @Test
     public void itCanLandAPlane() {
         airportUnderTest.land(planeUnderTest);
-        assertThat(airportUnderTest.getPlanes().contains(planeUnderTest));
+        assertThat(landedPlanes.contains(planeUnderTest));
     }
 
     @Test
@@ -25,6 +29,6 @@ public class AirportTest {
         airportUnderTest.land(planeUnderTest);
         Plane anotherPlane = new Plane();
         airportUnderTest.land(anotherPlane);
-        assertThat(airportUnderTest.getPlanes().contains(anotherPlane));
+        assertThat(landedPlanes.contains(anotherPlane));
     }
 }
